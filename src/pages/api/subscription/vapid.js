e431713051a224e13b4e5eb77c2beabb0961ce16
Jerome Wang immjs <informathemusic@gmail.com>
2023-06-03 22:57:10 +0200

    Add VAPID keys mechanism

---
+++ src/pages/api/subscription/vapid.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      return res.status(200).send(process.env.VAPID_PUBLIC);
+    default:
+      return res.status(405).send('Method not allowed');
+  }
+}
